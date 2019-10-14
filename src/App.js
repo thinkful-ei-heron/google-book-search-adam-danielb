207b36b74867fd828de5b1a6291b2e3db33651c3
Adam Newhouser <adamnewhouser@gmail.com>
2019-10-14 11:06:04 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+
+    return (
+      <main className='App'>
+        <header className='App-title'>
+          <h1>Google Book Search</h1>
+          <form className='search-filter-form'>
+            <div className='search-bar'>
+              <label for='search'>Search:</label>
+              <input id=''
+            </div>
+          </form>
+        </header>
+
+      </main>
+    );
+  }
+}
+
+export default App;
